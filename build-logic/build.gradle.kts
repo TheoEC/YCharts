3b57c71595a41d712a8cf884e52180f995305102
theo <theo_portela@hotmail.com>
2024-08-08 23:40:06 -0300

    Maven Java Version Error

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -10,8 +10,8 @@ repositories {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
@@ -21,7 +21,7 @@ dependencies {
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 }
 gradlePlugin {
--- build-logic/src/main/java/ycharts/android/KotlinAndroid.kt
+++ build-logic/src/main/java/ycharts/android/KotlinAndroid.kt
@@ -34,14 +34,14 @@ internal fun Project.configureKotlinAndroid(
             }
         }
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
         }
         // Use withType to workaround https://youtrack.jetbrains.com/issue/KT-55947
         tasks.withType<KotlinCompile>().configureEach {
             kotlinOptions {
                 // Set JVM target to 17
-                jvmTarget = JavaVersion.VERSION_11.toString()
+                jvmTarget = JavaVersion.VERSION_17.toString()
                 // Treat all Kotlin warnings as errors (disabled by default)
                 // Override by setting warningsAsErrors=true in your ~/.gradle/gradle.properties
                 val warningsAsErrors: String? by project
